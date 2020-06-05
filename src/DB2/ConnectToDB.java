f200c1d771af87fc911a99483713072ed832f1fa
DanieleCesarano96 <45485079+DanieleCesarano96@users.noreply.github.com>
2020-06-05 16:47:53 +0200

    .

---
+++ src/DB2/ConnectToDB.java
@@ -0,0 +1,51 @@
+package DB2;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+public class ConnectToDB {
+
+	private MongoClient mongo;
+	private MongoDatabase database;
+	
+	public ConnectToDB() {
+		mongo= null;
+		database= null;
+	}
+	
+	public Boolean Connesione () {
+		 mongo= new MongoClient("localhost", 27017);
+		 if(mongo==null) 
+		 {
+			 return false;
+		 }
+		 else 
+		 {
+			 database = mongo.getDatabase("BD2");
+			if (database==null) 
+			{
+				return false;
+			}
+			else 
+			{
+				return true;
+			}
+		}
+	}
+
+	public MongoClient getMongo() {
+		return mongo;
+	}
+
+	public void setMongo(MongoClient mongo) {
+		this.mongo = mongo;
+	}
+
+	public MongoDatabase getDatabase() {
+		return database;
+	}
+
+	public void setDatabase(MongoDatabase database) {
+		this.database = database;
+	}
+}
+
